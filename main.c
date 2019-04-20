4eb29be35c56d11c5e7450f71d0369cf589667ce
Marcel <marcelkaemper862@gmail.com>
2019-04-20 23:45:53 +0200

    Added custom filename

--- main.c
+++ main.c
@@ -2,20 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-void copyFile(char * name);
+void copyFile(char * name, char * filename);
 
 int main(int argc, char ** argv){
 
 	char * home = getenv("HOME");
+
 	// Building path from command line arguments
-	copyFile(strcat(strcat(strcat(strcat(home,"/Documents/coding/projects/boilerplate-master/boilerplates/"),argv[1]),"/"),argv[2]));
+	copyFile(strcat(strcat(strcat(strcat(home,"/Documents/coding/projects/boilerplate-master/boilerplates/"),argv[2]),"/"),argv[3]), argv[1]);
 		
 	return 0;
 }
 
-void copyFile(char * name){
+void copyFile(char * name, char * filename){
 	FILE *file = fopen(name, "r");
-	FILE *newFile = fopen(strcat(getenv("PWD"),"/copy"), "w");
+	FILE *newFile = fopen(strcat(strcat(getenv("PWD"),"/"),filename), "w");
 	
 	if (file != NULL){
 		char line [128];
