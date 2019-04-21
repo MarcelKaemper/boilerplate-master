5e1c89397a60b8ca8d98d2fbdcda5d62ef6b2d40
Marcel <marcelkaemper862@gmail.com>
2019-04-21 11:45:13 +0200

    Copying help file

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@ echo Running setup
 echo Creating directory ~/bpm/
 mkdir ~/.bpm/
 mkdir ~/.bpm/boilerplates
+echo Copying help
+cp ./help ~/.bpm/help
 echo Copying default boilerplates
 cp -r ./boilerplates/* ~/.bpm/boilerplates/
 echo Setup successful
