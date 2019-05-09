f55c0d73951b73e2b94797a2d89686948292df6f
Marcel <marcelkaemper862@gmail.com>
2019-05-09 15:43:47 +0200

    getDir function

--- main.c
+++ main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
 
 void copyFile(char * name, char * filename);
+void getDir(char * path);
 
 int main(int argc, char ** argv){
 
 	char * home = getenv("HOME");
+	char * path = strcat(home, "/.bpm/boilerplates/");
 
 	if(strcmp(argv[1],"help")){
 		// Building path from command line arguments
 		copyFile(strcat(strcat(strcat(strcat(home,"/.bpm/boilerplates/"),argv[2]),"/"),argv[3]), argv[1]);
 	}else{
+		getDir(path);
 		FILE *file = fopen(strcat(home, "/.bpm/help"), "r");
 		
 		if (file != NULL){
@@ -24,6 +28,20 @@ int main(int argc, char ** argv){
 	}
 		
 	return 0;
+}
+
+void getDir(char * path){
+	DIR *dp;
+	struct dirent *ep;	
+
+	dp = opendir(path);
+
+	if(dp != NULL){
+		while(ep = readdir(dp)){
+			puts(ep->d_name)	;
+		}
+		(void) closedir(dp);
+	}
 }
 
 void copyFile(char * name, char * filename){
